28066ae85499ebec5f81bf5958ee26a0722e090f
Anton Ukhanev <xedin.unknown@gmail.com>
2016-11-10 16:25:31 +0100

    Replaced next version placeholders

--- src/AggregatorInterface.php
+++ src/AggregatorInterface.php
@@ -7,14 +7,14 @@ namespace Dhii\Stats;
  *
  * A stats aggregator is something that calculates totals based on individual values.
  *
- * @since [*next-version*]
+ * @since 0.1.0
  */
 interface AggregatorInterface
 {
     /**
      * Aggregates stats from a set of items.
      *
-     * @since [*next-version*]
+     * @since 0.1.0
      *
      * @param mixed[]              $totals Existing totals.
      * @param mixed[]|\Traversable $items  A list of items to aggregate the totals from.
